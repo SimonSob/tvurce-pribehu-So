fd5ae8748ca4fb6dcdc8ac9a072fd3fcb29078fb
Kaňkovský Adam <kankovsky@prumyslovkajihlava.cz>
2025-01-27 12:25:03 +0100

    StoryPart component

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,23 +1,17 @@
-import { Divider, Typography } from '@mui/material';
+import { Divider } from '@mui/material';
 import Grid from '@mui/material/Grid2';
+import { StoryPart } from './StoryPart.jsx';
+import { StoryPartEdit } from './StoryPartEdit.jsx';
 
 export const MainPage = ({zacatek, zvrat, konec}) => {
     return (
     <Grid container spacing={2}>
-        <Grid size={10}>
-          <Typography variant='h2'>Zacatek příběhu</Typography>
-          <Typography variant='h4'>{zacatek}</Typography>
-        </Grid>
+        <StoryPart label="Zacatek pribehu" storyPart={zacatek} />
         <Divider />
-        <Grid size={10}>
-          <Typography variant='h2'>Zvrat</Typography>
-          <Typography variant='h4'>{zvrat}</Typography>
-        </Grid>
+        <StoryPart label="Zvrat" storyPart={zvrat} />
         <Divider />
-        <Grid size={10}>
-          <Typography variant='h2'>Konec příběhu</Typography>
-          <Typography variant='h4'>{konec}</Typography>
-        </Grid>
+        <StoryPart label="Konec pribehu" storyPart={konec} />
+        <StoryPartEdit label="Zacatek pribehu" />
       </Grid>
     );
 };
---
+++ src/components/StoryPartEdit.jsx
@@ -0,0 +1,11 @@
+import { Typography, TextField } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+
+export const StoryPartEdit = ({ label, storyPart }) => {
+    return (
+        <Grid size={10}>
+          <TextField id="storyPart" label={label} variant="outlined" multiline minRows={4} fullWidth />
+          <Typography variant='h4'>{storyPart}</Typography>
+        </Grid>
+    )
+}
